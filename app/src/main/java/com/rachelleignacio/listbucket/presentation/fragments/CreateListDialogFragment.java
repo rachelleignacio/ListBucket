413e738e24c10b05b38d7bdea9f42fe74b2386c0
rachelleignacio <ignacio.chelle88@gmail.com>
2017-04-22 15:26:42 -0700

    auto show and hide keyboard as needed when creating new list

--- app/src/main/java/com/rachelleignacio/listbucket/presentation/fragments/CreateListDialogFragment.java
+++ app/src/main/java/com/rachelleignacio/listbucket/presentation/fragments/CreateListDialogFragment.java
@@ -2,6 +2,7 @@ package com.rachelleignacio.listbucket.presentation.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -10,7 +11,9 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,10 +48,31 @@ public class CreateListDialogFragment extends DialogFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_fragment_create_list, null);
 
         editTextBox = (EditText) view.findViewById(R.id.create_list_edittext);
+        editTextBox.requestFocus();
+        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        showKeyboard(imm);
+        editTextBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    ((AlertDialog)getDialog()).getButton(AlertDialog.BUTTON_POSITIVE).callOnClick();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
         presenter = new CreateListFragmentPresenterImpl(ThreadExecutor.getInstance(),
                 MainThreadImpl.getInstance(), callback, DbInteractor.getInstance());
 
@@ -68,10 +92,19 @@ public class CreateListDialogFragment extends DialogFragment {
                             Toast.LENGTH_SHORT).show();
                 } else {
                     presenter.createList(editTextBox.getText().toString());
+                    hideKeyboard(imm);
                     dismiss();
                 }
             }
         });
         return builder.create();
+    }
+
+    private void showKeyboard(InputMethodManager imm) {
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    private void hideKeyboard(InputMethodManager imm) {
+        imm.hideSoftInputFromWindow(editTextBox.getWindowToken(), 0);
     }
 }
--- app/src/main/java/com/rachelleignacio/listbucket/presentation/fragments/RenameListDialogFragment.java
+++ app/src/main/java/com/rachelleignacio/listbucket/presentation/fragments/RenameListDialogFragment.java
@@ -2,13 +2,16 @@ package com.rachelleignacio.listbucket.presentation.fragments;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,23 +49,28 @@ public class RenameListDialogFragment extends DialogFragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_fragment_rename_list, null);
 
         editTextBox = (EditText) view.findViewById(R.id.rename_list_edittext);
-
-        // TODO: make this work properly
-//        editTextBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-//                boolean handled = false;
-//                if (actionId == EditorInfo.IME_ACTION_GO) {
-//                    view.findViewById(AlertDialog.BUTTON_POSITIVE).callOnClick();
-//                    handled = true;
-//                }
-//                return handled;
-//            }
-//        });
+//        editTextBox.requestFocus();
+        editTextBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    ((AlertDialog)getDialog()).getButton(AlertDialog.BUTTON_POSITIVE).callOnClick();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
 
         presenter = new RenameListFragmentPresenterImpl(ThreadExecutor.getInstance(),
                 MainThreadImpl.getInstance(), callback, DbInteractor.getInstance());
