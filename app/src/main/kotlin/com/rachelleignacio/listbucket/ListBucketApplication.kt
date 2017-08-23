a868518b85df4063942a5d52cf9523b1bfc3d577
rachelleignacio <ignacio.chelle88@gmail.com>
2017-08-23 12:48:03 -0700

    migrate ListBucketApplication to Kotlin

---
+++ app/src/main/kotlin/com/rachelleignacio/listbucket/ListBucketApplication.kt
@@ -0,0 +1,19 @@
+package com.rachelleignacio.listbucket
+
+import android.app.Application
+import com.raizlabs.android.dbflow.config.FlowConfig
+import com.raizlabs.android.dbflow.config.FlowLog
+import com.raizlabs.android.dbflow.config.FlowManager
+
+/**
+ * Created by rachelleignacio on 8/23/17.
+ */
+class ListBucketApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FlowManager.init(FlowConfig.Builder(this)
+                .openDatabasesOnInit(true)
+                .build())
+        FlowLog.setMinimumLoggingLevel(FlowLog.Level.V)
+    }
+}
