ea9bb6b7c61378fdb00b91286e8fa8a286900885
rachelleignacio <ignacio.chelle88@gmail.com>
2018-08-05 13:52:01 -0700

    kotlin cleanup in adapter classes

--- app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/adapters/ListItemsAdapter.kt
+++ app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/adapters/ListItemsAdapter.kt
@@ -18,24 +18,19 @@ class ListItemsAdapter(private val listItems: MutableList<ListItem>,
     : RecyclerView.Adapter<ListItemsAdapter.ViewHolder>(), ListAdapterTouchListener {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent!!.context)
-                .inflate(R.layout.list_item_layout, parent, false)
-        return ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            LayoutInflater
+                    .from(parent.context)
+                    .inflate(R.layout.list_item_layout, parent, false)
+                    .run { ViewHolder(this) }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.listItemNameTv.text = listItems[position].name
     }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        val listItem = listItems[position]
-        holder!!.listItemNameTv.text = listItem.name
-    }
+    override fun getItemCount() = listItems.size
 
-    override fun getItemCount(): Int {
-        return listItems.size
-    }
-
-    override fun onRowDismiss(position: Int) {
-        view.onItemSwipedToDelete(position)
-    }
+    override fun onRowDismiss(position: Int) { view.onItemSwipedToDelete(position) }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val listItemNameTv: TextView = itemView.findViewById(R.id.item_name)
--- app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/adapters/ListsAdapter.kt
+++ app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/adapters/ListsAdapter.kt
@@ -18,27 +18,23 @@ class ListsAdapter(private val lists: MutableList<List>, private val view: ListB
         : RecyclerView.Adapter<ListsAdapter.ViewHolder>(), ListAdapterTouchListener {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.list_bucket_item_layout,
-                parent, false)
-        return ViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.list_bucket_item_layout, parent, false)
+                .run { ViewHolder(this) }
 
-    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val list = lists[position]
-        holder!!.listNameTv.text = list.name
+        holder.listNameTv.text = list.name
         holder.renameListIv.setOnClickListener { view.onClickRenameList(list) }
         holder.itemView.setOnClickListener { view.onClickList(list) }
     }
 
-    override fun getItemCount(): Int {
-        return lists.size
-    }
+    override fun getItemCount() = lists.size
 
 
-    override fun onRowDismiss(position: Int) {
-        view.onListSwipedToDelete(position)
-    }
+    override fun onRowDismiss(position: Int) { view.onListSwipedToDelete(position) }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var listNameTv: TextView = itemView.findViewById(R.id.list_name)
