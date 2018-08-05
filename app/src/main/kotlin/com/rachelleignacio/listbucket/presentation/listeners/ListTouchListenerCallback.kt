89937d30990a4d12c6d41f740e6f8f8f0675ee90
rachelleignacio <ignacio.chelle88@gmail.com>
2018-08-05 13:55:11 -0700

    cleanup kotlin code in touch listener callback for swipe to delete

--- app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/listeners/ListTouchListenerCallback.kt
+++ app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/listeners/ListTouchListenerCallback.kt
@@ -8,25 +8,15 @@ import android.support.v7.widget.helper.ItemTouchHelper
  */
 class ListTouchListenerCallback(private val listTouchListener: ListAdapterTouchListener) : ItemTouchHelper.Callback() {
 
-    override fun isLongPressDragEnabled(): Boolean {
-        return false
-    }
+    override fun isLongPressDragEnabled() = false
 
-    override fun isItemViewSwipeEnabled(): Boolean {
-        return true
-    }
+    override fun isItemViewSwipeEnabled() = true
 
-    override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?): Int {
-        val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
-        return makeMovementFlags(0, swipeFlags)
-    }
+    override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?) =
+        makeMovementFlags(0, ItemTouchHelper.START or ItemTouchHelper.END)
 
-    override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
-        if (viewHolder!!.itemViewType != target!!.itemViewType) {
-            return false
-        }
-        return true
-    }
+    override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?) =
+        (viewHolder!!.itemViewType == target!!.itemViewType)
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
         listTouchListener.onRowDismiss(viewHolder!!.adapterPosition)
