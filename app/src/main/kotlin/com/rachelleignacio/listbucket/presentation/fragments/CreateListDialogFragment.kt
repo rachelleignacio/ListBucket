631c5df770bac9aa65761e7ed62a6d6215f0b40b
rachelleignacio <ignacio.chelle88@gmail.com>
2018-08-05 14:31:23 -0700

    refactor CreateListDialogFragment

--- app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/fragments/CreateListDialogFragment.kt
+++ app/src/main/kotlin/com/rachelleignacio/listbucket/presentation/fragments/CreateListDialogFragment.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import android.widget.Toast
@@ -29,33 +30,53 @@ class CreateListDialogFragment @SuppressLint("ValidFragment") internal construct
     internal lateinit var callback: CreateListInteractor.Callback
     private lateinit var presenter: CreateListFragmentPresenter
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val view = activity.layoutInflater.inflate(R.layout.dialog_fragment_create_list, null)
+    override fun onCreateDialog(savedInstanceState: Bundle?) =
+            initPresenter()
+                    .run { inflateView() }
+                    .applyToAlertDialogAndCreate()
+                    .setDialogButtonClickListeners()
 
-        editTextBox = view.findViewById(R.id.create_list_edittext)
-        editTextBox.requestFocus()
-        Keyboard.show(activity, editTextBox)
-        editTextBox.setOnEditorActionListener { _, actionId, _ ->
-            var handled = false
-            if (actionId == EditorInfo.IME_ACTION_GO) {
-                (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).callOnClick()
-                handled = true
-            }
-            handled
-        }
-
-
+    private fun initPresenter() {
         presenter = CreateListFragmentPresenterImpl(ThreadExecutorImpl, MainThreadImpl, callback, DbInteractor)
+    }
 
-        val builder = AlertDialog.Builder(activity)
-        builder.setView(view)
-        builder.setNegativeButton(getString(android.R.string.cancel), null)
-        builder.setPositiveButton(getString(R.string.create_list_dialog_submit), null)
+    private fun inflateView(): View =
+        activity
+                .layoutInflater
+                .inflate(R.layout.dialog_fragment_create_list, null)
+                .apply { initEditTextBox() }
 
-        val dialog: AlertDialog = builder.create()
-        dialog.setOnShowListener {
-            dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener { dismiss() }
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+    private fun View.initEditTextBox() {
+        editTextBox = findViewById<EditText>(R.id.create_list_edittext).apply {
+            requestFocus()
+            Keyboard.show(activity, this)
+            setOnEditorActionListener { _, actionId, _ ->
+                var handled = false
+                if (actionId == EditorInfo.IME_ACTION_GO) {
+                    (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).callOnClick()
+                    handled = true
+                }
+                handled
+            }
+        }
+    }
+
+    private fun View.applyToAlertDialogAndCreate() =
+            AlertDialog.Builder(activity)
+                    .setButtonTextAndApplyView(this)
+                    .create()
+
+    private fun AlertDialog.Builder.setButtonTextAndApplyView(view: View) =
+            apply {
+                setView(view)
+                setNegativeButton(getString(android.R.string.cancel), null)
+                setPositiveButton(getString(R.string.create_list_dialog_submit), null)
+            }
+
+    private fun AlertDialog.setDialogButtonClickListeners() = apply {
+        setOnShowListener {
+            getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener { dismiss() }
+            getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
                 if (editTextBox.text.isEmpty()) {
                     Toast.makeText(activity, R.string.create_list_error_toast_msg, Toast.LENGTH_SHORT).show()
                 } else {
@@ -64,8 +85,6 @@ class CreateListDialogFragment @SuppressLint("ValidFragment") internal construct
                 }
             }
         }
-
-        return dialog
     }
 
     companion object {
